cfed882d1de70eaa6ac68a1b0191d0a442f88dc6
denhateu <danilaksenow101@gmail.com>
2025-06-04 00:01:02 +0500

    feat: added simple code for telegram 'echo' bot

--- aihelperbot/main.py
+++ aihelperbot/main.py
@@ -1,6 +1,49 @@
-def main():
-    print("huy")
+import sys
+import logging
+from os import getenv
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+
+# Gets token from environment
+TOKEN = getenv("TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+
+    await message.answer("Хай")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Хорошая попытка!")
+
+
+async def main() -> None:
+    # Run events dispatching
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
