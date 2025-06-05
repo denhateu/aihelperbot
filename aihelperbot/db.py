04087f84b88748ec14b61c2b3b4cddf7501e9c2e
denhateu <danilaksenow101@gmail.com>
2025-06-05 17:47:03 +0500

    feat: added function for add data to database

--- aihelperbot/db.py
+++ aihelperbot/db.py
@@ -5,12 +5,21 @@ class Database:
     def __init__(self, database_name: str) -> None:
         # Connecting to database
         self.connection = sqlite3.connect(database_name)
+        self.cursor = self.connection.cursor()
 
     def execute_query(self, query: str) -> None:
-        cursor = self.connection.cursor()
-
         # Executing query
-        cursor.execute(query)
+        self.cursor.execute(query)
+
+        # Saves changes
+        self.connection.commit()
+
+    def add_data(self, table_name: str, column_name: str, data) -> None:
+        # Executing query
+        self.cursor.execute(f"""
+INSERT INTO {table_name} ({column_name})
+VALUES (?)
+""", (data,))
 
         # Saves changes
         self.connection.commit()
