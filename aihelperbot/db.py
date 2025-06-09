028ae7b7229e6382ccada9d68fe63678e2c1a2ed
denhateu <danilaksenow101@gmail.com>
2025-06-09 17:26:11 +0500

    chore: added new fetch_all() and fetch_one() methods

--- aihelperbot/db.py
+++ aihelperbot/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from typing import Tuple, Any, Optional
 
 
 class Database:
@@ -7,17 +8,22 @@ class Database:
         self.connection = sqlite3.connect(database_name)
         self.cursor = self.connection.cursor()
 
-    def execute_query(self, query: str, data = None) -> list:
+    def execute_query(self, query: str, params: Optional[Tuple[Any, ...]] = None) -> None:
         # Executing query
-        if data:
-            self.cursor.execute(query, (data))
-        else:
-            self.cursor.execute(query)
+        self.cursor.execute(query, params or ())
 
         # Saves changes
         self.connection.commit()
 
+    def fetch_all(self, query: str, params: Optional[Tuple[Any, ...]] = None) -> list:
+        # Executing query
+        self.cursor.execute(query, params or ())
         return self.cursor.fetchall()
+
+    def fetch_one(self, query: str, params: Optional[Tuple[Any, ...]] = None) -> list:
+        # Executing query
+        self.cursor.execute(query, params or ())
+        return self.cursor.fetchone()
 
     def close(self) -> None:
         # Closing database connection
