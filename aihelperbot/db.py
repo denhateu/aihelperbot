86cad368e8d86a03c169a8d45970911b6ea9392c
denhateu <danilaksenow101@gmail.com>
2025-06-10 14:04:41 +0500

    docs: added docstrings for class and methods

--- aihelperbot/db.py
+++ aihelperbot/db.py
@@ -3,28 +3,89 @@ from typing import Tuple, Any, Optional
 
 
 class Database:
+    """
+    This is simple database manager class, this class including methods
+    for only execute query, execute query and fetch one record and
+    execute query and fetch all records
+    """
+
     def __init__(self, database_name: str) -> None:
+        """
+        Initialize connection to database and creates cursor object for
+        database management
+
+        Args:
+            database_name:
+                The name of the database to which you want to connect
+        """
+
         # Connecting to database
         self.connection = sqlite3.connect(database_name)
         self.cursor = self.connection.cursor()
 
-    def execute_query(self, query: str, params: Optional[Tuple[Any, ...]] = None) -> None:
+    def execute_query(self, query: str,
+                      params: Optional[Tuple[Any, ...]] = None) -> None:
+        """
+        Executing SQL query with parameters, but parameters are not
+        required
+
+        Args:
+            query:
+                SQL query string
+            params:
+                Various data that are needed to fulfill the request
+        """
+
         # Executing query
         self.cursor.execute(query, params or ())
 
         # Saves changes
         self.connection.commit()
 
-    def fetch_all(self, query: str, params: Optional[Tuple[Any, ...]] = None) -> list:
+    def fetch_all(self, query: str,
+                  params: Optional[Tuple[Any, ...]] = None) -> list:
+        """
+        Executing SQL query with parameters, but parameters are not
+        required
+
+        Args:
+            query:
+                SQL query string
+            params:
+                Various data that are needed to fulfill the request
+
+        Returns:
+            List of all got elements from table
+        """
+
         # Executing query
         self.cursor.execute(query, params or ())
         return self.cursor.fetchall()
 
-    def fetch_one(self, query: str, params: Optional[Tuple[Any, ...]] = None) -> list:
+    def fetch_one(self, query: str,
+                  params: Optional[Tuple[Any, ...]] = None) -> list:
+        """
+        Executing SQL query with parameters, but parameters are not
+        required
+
+        Args:
+            query:
+                SQL query string
+            params:
+                Various data that are needed to fulfill the request
+
+        Returns:
+            A list containing the very first element found
+        """
+
         # Executing query
         self.cursor.execute(query, params or ())
         return self.cursor.fetchone()
 
     def close(self) -> None:
+        """
+        Closes database connection
+        """
+
         # Closing database connection
         self.connection.close()
--- aihelperbot/task.py
+++ aihelperbot/task.py
@@ -3,7 +3,17 @@ from config_parser import config
 
 
 class Task:
+    """
+    This is class for manage tasks, including create, read,
+    edit, delete and etc
+    """
+
     def __init__(self) -> None:
+        """
+        When class initializing, then create tasks table if this table
+        not exists in database
+        """
+
         # Initialize database connection
         db = Database(config["database"]["name"])
 
@@ -18,6 +28,18 @@ CREATE TABLE IF NOT EXISTS tasks (
         db.close()
 
     def task_id_exists(self, task_id: str) -> bool:
+        """
+        Checks if task ID is exists in database
+
+        Args:
+            task_id:
+                Task identificator, this is number from 1 to ...
+
+        Returns:
+            True - if task ID exists in database
+            False - if task ID not exists in database
+        """
+
         # Initialize database connection
         db = Database(config["database"]["name"])
         task = db.fetch_one(f"SELECT * FROM tasks WHERE id = ?", (task_id,))
@@ -29,6 +51,18 @@ CREATE TABLE IF NOT EXISTS tasks (
             return True
 
     def task_name_exists(self, task_name: str) -> bool:
+        """
+        Checks if task name is exists in database
+
+        Args:
+            task_name:
+                This is any string
+
+        Returns:
+            True - if task name exists in database
+            False - if task name not exists in database
+        """
+
         # Initialize database connection
         db = Database(config["database"]["name"])
         task = db.fetch_one(f"SELECT * FROM tasks WHERE name = ?", (task_name,))
@@ -40,6 +74,14 @@ CREATE TABLE IF NOT EXISTS tasks (
             return True
 
     def create_task(self, task_name: str) -> None:
+        """
+        Adds a new task to the table
+
+        Args:
+            task_name:
+                Any string
+        """
+
         # Initialize database connection
         db = Database(config["database"]["name"])
 
@@ -51,6 +93,16 @@ VALUES (?)
         db.close()
 
     def edit_task(self, task_id: str, task_name: str) -> None:
+        """
+        Changes task name from old name to new name in table using task ID
+
+        Args:
+            task_id:
+                Task identificator, this is number from 1 to ...
+            task_name:
+                Any string
+        """
+
         # Initialize database connection
         db = Database(config["database"]["name"])
 
@@ -63,6 +115,14 @@ WHERE id = ?
         db.close()
 
     def delete_task(self, task_id: str) -> None:
+        """
+        Removes task from table using task ID
+
+        Args:
+            task_id:
+                Task identificator, this is number from 1 to ...
+        """
+
         # Initialize database connection
         db = Database(config["database"]["name"])
 
@@ -74,6 +134,13 @@ WHERE id = ?
         db.close()
 
     def get_all_tasks(self) -> list:
+        """
+        Reads all tasks from table
+
+        Returns:
+            List of tasks, including ID and name
+        """
+
         # Initialize database connection
         db = Database(config["database"]["name"])
         tasks = db.fetch_all("SELECT * FROM tasks")
